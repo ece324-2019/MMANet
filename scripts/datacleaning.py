d35bb040874dffcd39d67cbd81d8b40e3da0ea48
Rui Gao <ruiqiu.gao@mail.utoronto.ca>
2019-11-12 15:14:26 -0500

    Adding comments

--- scripts/datacleaning.py
+++ scripts/datacleaning.py
@@ -1,7 +1,8 @@
-'''
+"""
 Code for data cleaning
 Balanced Red and Blue Wins
-'''
+"""
+
 import numpy as np
 import pandas as pd
 from models import *
@@ -9,29 +10,29 @@ from models import *
 np.random.seed(420)
 data = pd.read_csv("preprocessed_data.csv")
 data = data.drop(columns=["B_Stance_Sideways", "title_bout"])
-datared = data[data["Winner"] == "Red"].copy()
-datablue = data[data["Winner"] == "Blue"].copy()
+data_red = data[data["Winner"] == "Red"].copy()
+data_blue = data[data["Winner"] == "Blue"].copy()
 
-datablue = datablue.reset_index(drop=True)
-datared = datared.reset_index(drop=True)
-indices = list(range(len(datared)))
+data_blue = data_blue.reset_index(drop=True)
+data_red = data_red.reset_index(drop=True)
+indices = list(range(len(data_red)))
 np.random.shuffle(indices)
-dataredfinal = datared[datared.index.isin(indices[:1796])].copy()
+data_red_final = data_red[data_red.index.isin(indices[:1796])].copy()
 
-dataredtoblue = datared[datared.index.isin(indices[1796:])].copy()
-dataredtoblue.loc[:, "Winner"] = "Blue"
-windata = dataredtoblue.iloc[:, 0:2]
-bluestats = dataredtoblue.iloc[:, 2:68]
-redstats = dataredtoblue.iloc[:, 68:134]
-blueage = dataredtoblue.iloc[:, 134:135]
-redage = dataredtoblue.iloc[:, 135:136]
-weights = dataredtoblue.iloc[:, 136:150]
-bluestance = dataredtoblue.iloc[:, 150:154]
-redstance = dataredtoblue.iloc[:, 154:158]
-dataredtobluefinal = windata.join(redstats).join(bluestats).join(redage).join(blueage).join(weights).join(
+data_red_to_blue = data_red[data_red.index.isin(indices[1796:])].copy()
+data_red_to_blue.loc[:, "Winner"] = "Blue"
+windata = data_red_to_blue.iloc[:, 0:2]
+bluestats = data_red_to_blue.iloc[:, 2:68]
+redstats = data_red_to_blue.iloc[:, 68:134]
+blueage = data_red_to_blue.iloc[:, 134:135]
+redage = data_red_to_blue.iloc[:, 135:136]
+weights = data_red_to_blue.iloc[:, 136:150]
+bluestance = data_red_to_blue.iloc[:, 150:154]
+redstance = data_red_to_blue.iloc[:, 154:158]
+data_red_to_bluefinal = windata.join(redstats).join(bluestats).join(redage).join(blueage).join(weights).join(
     redstance).join(bluestance)
-dataredtobluefinal.columns = list(dataredfinal.columns)
-datafinal = pd.concat([datablue, dataredtobluefinal, dataredfinal], axis=0)
+data_red_to_bluefinal.columns = list(data_red_final.columns)
+datafinal = pd.concat([data_blue, data_red_to_bluefinal, data_red_final], axis=0)
 datafinal.loc[:, "Winner"] = (datafinal["Winner"] == "Red").astype(int)
 datafinal = datafinal.sample(frac=1,random_state=420).reset_index(drop=True)
 datafinal.to_csv("datafinal.csv", index=False)
